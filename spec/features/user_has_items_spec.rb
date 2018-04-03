1bb644110a80692969218d139308febae6c38e4b
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-03 11:27:13 -0600

    Adds spec for user having items

--- spec/features/user_has_items_spec.rb
+++ spec/features/user_has_items_spec.rb
@@ -3,8 +3,19 @@ require 'rails_helper'
 describe 'items' do
   scenario 'user sees items in show page' do
     user = User.create(username: 'Kelly Schroeder', email: 'krschroeder@gmail.com', password: 'one2three4five6')
-    item = Item.create(name: 'Sword', type: 'Weapon', offense: 1, defense: 0)
-    
+    # stub current_user here for simulated login
+    item = Item.create(name: 'Sword', item_type: 'Weapon', offense: 1, defense: 0)
+    visit '/'
+
+    click_on 'Sign In'
+
+    expect(current_path).to eq(login_path)
+
+    fill_in 'username', with: user.username
+    fill_in 'password', with: user.password
+
+    click_on 'Log In'
+
     visit user_path(user)
 
     expect(page).to have_content('Your Items')
