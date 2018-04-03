43da91585b207790db7c367194ae5b74b108b712
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-03 12:08:48 -0600

    Adds authorization functionality for requiring a user to be an admin for admin functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,20 @@ describe User do
     it {should validate_uniqueness_of(:username)}
     it {should validate_uniqueness_of(:email)}
   end
+
+  describe 'roles' do
+    it 'can be an admin' do
+      admin = User.create!(username: 'Anubis Khan', email: 'poweroverwhelming@godmode.com', password: 'blacksheepwall', role: 1)
+
+      expect(admin.admin?).to be_truthy
+      expect(admin.role).to eq('admin')
+    end
+
+    it 'can be a default user' do
+      reg_user = User.create!(username: 'newbtube', email: 'ff@20.com', password: 'l2p')
+
+      expect(reg_user.default?).to be_truthy
+      expect(reg_user.role).to eq('default')
+    end
+  end
 end
