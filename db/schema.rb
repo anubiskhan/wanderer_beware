bcc332f7f84c10550bacd8bd1068250f6552cfcb
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-02 21:51:30 -0600

    Adds items migration, and migrates

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403033559) do
+ActiveRecord::Schema.define(version: 20180403035105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "item_type"
     t.integer "offense"
     t.integer "defense"
     t.datetime "created_at", null: false
