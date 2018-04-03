f17a2185e530f017cc6ff71c3ebbb0983374081c
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-03 11:30:26 -0600

    Adds validation for uniqueness of username and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,7 @@ describe User do
   describe 'validations' do
     it {should validate_presence_of(:username)}
     it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_uniqueness_of(:email)}
   end
 end
