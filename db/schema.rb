c321a46ab0a4ac0c421db5c5e772f5362aa24d62
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-03 19:00:52 -0600

    Adds stories migration and migrates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404001155) do
+ActiveRecord::Schema.define(version: 20180404005707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20180404001155) do
     t.integer "offense"
     t.integer "defense"
     t.integer "health"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
